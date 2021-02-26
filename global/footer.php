5cada55b3d7630951c44b45e82eaaeb3af04d3a0
zaeemdarwin <shrikunj.vyas@darwinpgc.com>
2021-02-26 18:28:56 +0530

    bugs resolved

--- global/footer.php
+++ global/footer.php
@@ -1,8 +1,5 @@
 <footer class="clearfix">
-<!--    <div class="pull-right">-->
-<!--        Crafted with <i class="fa fa-heart text-danger"></i> by <a href="https://1.envato.market/ydb" target="_blank">pixelcave</a>-->
-<!--    </div>-->
     <div class="pull-left">
-        <span >2020</span> &copy; <a href="https://darwintrip.com/" target="_blank">DarwinTrip</a>
+        <span ><?php echo date('Y') ?></span> &copy; <a href="https://darwintrip.com/" target="_blank">DarwinTrip</a>
     </div>
 </footer>
